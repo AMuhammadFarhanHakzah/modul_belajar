c3364342d204e687cab1ba5e9f9c4e451e7abc15
AMuhammadFarhanHakzah <amf21022002@gmail.com>
2024-04-20 10:02:09 +0800

    homepage

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Impact Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+@include('components.style')
+  <!-- =======================================================
+  * Template Name: Impact
+  * Template URL: https://bootstrapmade.com/impact-bootstrap-business-website-template/
+  * Updated: Apr 4 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+@yield('content')
+
+@include('components.script')
+
+</body>
+
+</html>
